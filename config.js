1792890f3492200d109a70c2d07d58914ddb9c0b
Anneki22 <168829607+Anneki22@users.noreply.github.com>
2024-12-29 11:59:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 
 global.owner = process.env.OWNER_NUMBER.split(','); 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "_ＮＥＭＥＳＩＸ";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.author = process.env.AUTHOR || "_ＮＥＭＥＳＩＸ";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
-global.autoTyping = process.env.AUTO_TYPING === 'true'; 
-global.autoRecord = process.env.AUTO_RECORD === 'true';
-global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
-global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
+global.autoTyping = process.env.AUTO_TYPING === 'false'; 
+global.autoRecord = process.env.AUTO_RECORD === 'false';
+global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'false';
+global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'false';
 global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
-global.ANTIVIEWONCE = process.env.ANTIVIEWONCE === 'true';
+global.ANTIVIEWONCE = process.env.ANTIVIEWONCE === 'false';
 
 global.roles = {
     1: "Novice",
@@ -30,22 +30,22 @@ global.roles = {
 
 global.sudoUsers = process.env.SUDO_USERS.split(',').map(user => `${user}@s.whatsapp.net`); 
 
-global.public = process.env.PUBLIC === 'true';
+global.public = process.env.PUBLIC === 'false';
 
-global.ANTIDELETE = process.env.ANTIDELETE === 'true';
+global.ANTIDELETE = process.env.ANTIDELETE === 'false';
 global.unavailable = process.env.UNAVAILABLE === 'true';
-global.available = process.env.AVAILABLE === 'true';
-global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
+global.available = process.env.AVAILABLE === 'false';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'false';
 global.chatbot = process.env.CHATBOT === 'true';
-global.autoreact = process.env.AUTO_REACT === 'true';
+global.autoreact = process.env.AUTO_REACT === 'false';
 
-global.WELCOME = process.env.WELCOME === 'true';
+global.WELCOME = process.env.WELCOME === 'false';
 
-global.prefix = process.env.PREFIX || '.';
+global.prefix = process.env.PREFIX || 'X';
 
-global.autobio = process.env.AUTO_BIO === 'true';
+global.autobio = process.env.AUTO_BIO === 'false';
 
-global.ANTICALL = process.env.ANTICALL === 'true';
+global.ANTICALL = process.env.ANTICALL === 'false';
 
 global.antilink = process.env.ANTILINK === 'true';
 global.antilinkkick = process.env.ANTILINK_KICK === 'true';
